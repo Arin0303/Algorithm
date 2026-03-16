428e7090e95a86b0b9ade1577fe6d0429262efa2
Arin <ape4246@gmail.com>
2026-03-16 22:28:49 +0900

    [Bronze III] Title: 팩토리얼, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -0,0 +1,18 @@
+"""
+N >= 0
+N!
+
+n=10
+10*9*8*...1
+"""
+
+# n = int(input())
+# result = 1
+# for i in range(n, 0, -1): 
+#     result *= i
+# print(result)
+
+import math
+
+n = int(input())
+print(math.factorial(n)) # n!
